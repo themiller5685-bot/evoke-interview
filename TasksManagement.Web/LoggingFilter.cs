043e440e7c03fba629609e64bc4447f7bd032f63
themiller5685@gmail.com <themiller5685@gmail.com>
2025-09-19 14:52:28 +0300

    Added simple logging filter

--- TasksManagement.Web/LoggingFilter.cs
+++ TasksManagement.Web/LoggingFilter.cs
@@ -3,12 +3,18 @@
 
 public class LoggingFilter : IEndpointFilter
 {
+    private ILogger<LoggingFilter> _logger;
+    public LoggingFilter(ILogger<LoggingFilter> logger)
+    {
+        _logger = logger;
+    }
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
-        //TODO: Implement logging filter
+        _logger.LogInformation("Incoming request for {Method} {Path} started at {Time}" , context.HttpContext.Request.Method, context.HttpContext.Request.Path, DateTime.Now);
         
         var response = await next(context);
 
+        _logger.LogInformation("Request {Method} {Path} ended at {Time}", context.HttpContext.Request.Method, context.HttpContext.Request.Path, DateTime.Now);
         return response;
     }
 }
